12e2107b93aec2b9716570745299c607dd8b2daa
liyao123 <1124863805@qq.com>
2019-07-01 12:37:35 +0800

    高并发:第二章学习

---
+++ src/cn/liyao52/concurrent/chapter02/DeamonThread.java
@@ -0,0 +1,22 @@
+package cn.liyao52.concurrent.chapter02;
+
+public class DeamonThread {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread thread = new Thread(()->{
+            while (true){
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+//         thread.setDaemon(true); // 将thread设置为守护线程
+
+        thread.start(); //启动Thread线程
+        Thread.sleep(2_000L);
+        System.out.println("Main thread finished lifecycle.");
+    }
+}
